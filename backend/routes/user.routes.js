b280ad08d8b8b27a33bf74da4dda1a65442f81c4
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-05-28 01:22:35 +0530

    Added jwt in signup endpoint

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,39 +1,64 @@
 const express = require("express");
 const User = require("../models/users.schema");
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+const signupBody = z.object({
+  username: z.string(),
+  password: z.string(),
+  firstName: z.string().min(3).max(50),
+  lastName: z.string().min(3).max(50),
+});
+
 router.post("/signup", async (req, res) => {
   try {
+    // Input checking
+    const { success } = signupBody.safeParse(req.body);
+
+    if (!success) {
+      return res.status(411).send({
+        msg: "Incorrect Inputs",
+      });
+    }
+
     const { username, password, firstName, lastName } = req.body;
-  const user = await User.findOne({
-    username: username,
-  });
-
-
-  if (!user) {
-    const user = await User.create({
+    const user = await User.findOne({
       username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
     });
 
-    res.status(200).send({
+    if (!user) {
+      const user = await User.create({
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+      });
+
+      const token = jwt.sign(
+        {
+          userId: user._id,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.status(200).send({
         MSG: "User created successfully!",
         userId: user._id,
-        username: user.username
-    })
-  } else {
-    res.status(400).send({
-        MSG: "User already exist!"
-    })
-  }
+        username: user.username,
+        token: token,
+      });
+    } else {
+      res.status(400).send({
+        MSG: "User already exist!",
+      });
+    }
   } catch (error) {
     res.status(500).send({
-        MSG: "Internal server error!",
-        error: error
-    })
+      MSG: "Internal server error!",
+      error: error,
+    });
   }
 });
 
